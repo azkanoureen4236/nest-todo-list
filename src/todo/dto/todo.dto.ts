0c807109dbac397e537be9bcbf3e4dc28052c4c6
azkanoureen4236 <azkanoureen4236@gmail.com>
2025-07-21 17:36:38 +0500

    add validationin todo dto

--- src/todo/dto/todo.dto.ts
+++ src/todo/dto/todo.dto.ts
@@ -1,7 +1,8 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class TodoDto {
   @IsNotEmpty({ message: 'Title is required' })
   title: string;
+  @IsString({message: 'description will be on string'})
   description?: string;
 }
